7f6d08dedc2f51396880505b8c12cd90bf301a9d
squawk7x <andreas@ottburg.de>
2023-06-25 11:14:41 +0200

    20230625-1100 Initial Commit - Just a Little Card Game

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+};
